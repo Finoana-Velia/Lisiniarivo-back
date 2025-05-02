22c05bc67e2f215b78265c0b0f8506234de7067d
Finoana-Velia <fypatrick20@yahoo.com>
2025-05-02 17:20:59 +0300

    feat update age category

--- src/main/java/com/Lisiniarivo/Application/Entity/Size.java
+++ src/main/java/com/Lisiniarivo/Application/Entity/Size.java
@@ -6,6 +6,9 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum Size {
-	ENFANT("Enfant"),S("S"),XS("XS"),M("M"),L("L"),XL("XL"),XXL("XXL");
+	//1-2-4-6-8-10-12 Enfant
+	UN_AN("1"),DEUX_ANS("2"),QUATRE_ANS("4"),
+	SIX_ANS("6"),HUIT_ANS("8"),DIX_ANS("10"),DOUZE_ANS("12"),
+	S("S"),XS("XS"),M("M"),L("L"),XL("XL"),XXL("XXL"),XXXL("XXXL");
 	private String value;
 }
